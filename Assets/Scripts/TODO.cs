1a4736fcc11e62270bddde72a8eb824214640d08
Andrew Romano <romanoa4@tcnj.edu>
2025-09-21 16:05:30 -0400

    TODO and Node changes

---
+++ Assets/Scripts/TODO.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+/*
+- Terminal
+Trees of nodes or just nodes
+Manually refresh the nodes makes the game harder
+Additional automatic refresh on a timer (forced to get stronger)
+2 Layers of defenses on nodes: Bypass firewall and extract
+Can't see reward until firewall is bypassed
+
+
+- Security
+Bar that fills up 1-10, if it reaches 10 you lose
+Minigames to fix things
+Timed event to reset VPN or bar increases over time
+AI hackers attack you. Use antivirus. This can drain your money until removed
+While hacking, if your actions fail, bar increases
+Reduce threat bar slowly over time
+Manual way to lower threat with a minigame (Identity spoof?)
+
+- Market
+Purchase higher levels of bypass, extractor, antivirus, searching (increases node value)
+Comes with a risk of purchasing something that hurts you. (Maybe requires antivirus)
+Opening the market increase your threat level
+
+- Logs/App Maintence
+Used to monitor and reboot the other apps which may go down during gameplay
+This must be done to regain access to the other apps
+Also used to monitor your threat level
+
+*/
