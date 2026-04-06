656656000a709a83a72b9887d8d7c5d7dc953922
Andrew Romano <romanoa4@tcnj.edu>
2026-04-06 18:41:11 -0400

    VPN Audio

--- Assets/Scripts/GameplayAudioManager.cs
+++ Assets/Scripts/GameplayAudioManager.cs
@@ -7,6 +7,8 @@ public class GameplayAudioManager : MonoBehaviour
 
     [FMODUnity.ParamRef]
     public string paymentTimerParam;
+    [FMODUnity.ParamRef]
+    public string vpnParam;
 
     void Start()
     {
--- Assets/Scripts/VPN.cs
+++ Assets/Scripts/VPN.cs
@@ -6,6 +6,9 @@ public class VPN : MonoBehaviour
     public TMP_Text vpnTimerText;
     public TMP_Text IPText;
 
+    // Audio
+    public GameplayAudioManager audioManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,11 +33,15 @@ public class VPN : MonoBehaviour
         else if (vpnTime < WARNING_VPN_TIME)
         {
             timerColor = "yellow";
+
         }
         else
         {
             timerColor = "red";
             GameManager.forceHackFail = true;
+
+            // Audio changes when VPN fails
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName(audioManager.vpnParam, 0);
         }
 
         vpnTimerText.text = "Time on current network: <color=\"" + timerColor + "\">" + vpnTime + "s</color>";
@@ -45,6 +52,9 @@ public class VPN : MonoBehaviour
         // Reset VPN
         GameManager.vpnTimer = 0;
         GameManager.forceHackFail = false;
+
+        // Audio changes when VPN is back on
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName(audioManager.vpnParam, 1);
 
         // Generate and set new IP text
         GenerateIP();
