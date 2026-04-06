bab02852ea491ee9aba9b12d790e752ad24816c1
Andrew Romano <romanoa4@tcnj.edu>
2026-04-06 16:38:53 -0400

    Audio Manager for gameplay music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using FMODUnity;
+
+public class AudioManager : MonoBehaviour
+{
+    public static StudioEventEmitter gameplayEmitter;
+    public static StudioEventEmitter startEmitter;
+
+    [FMODUnity.ParamRef]
+    public float game_start;
+
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        game_start = 1f;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -46,6 +46,7 @@ public class GameManager : MonoBehaviour
 
 
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
