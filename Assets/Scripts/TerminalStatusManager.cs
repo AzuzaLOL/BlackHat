ca884947d4954e5f8709cd1d7144081ca8ac74c8
Andrew Romano <romanoa4@tcnj.edu>
2026-04-22 20:40:09 -0400

    Static VFX. Terminal down reworked

--- Assets/Scripts/TerminalStatusManager.cs
+++ Assets/Scripts/TerminalStatusManager.cs
@@ -3,9 +3,11 @@ using UnityEngine.Events;
 
 public class TerminalStatusManager : MonoBehaviour
 {
-    public Terminal terminal;
-    public Typing typing;
-    public GameObject notWorkingCoverImage;
+    // public Terminal terminal;
+    // public Typing typing;
+    public GameObject[] objectsToDisable;
+    public GameObject notWorkingStaticCoverImage;
+    public GameObject notWorkingBorderCoverImage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,15 +20,25 @@ public class TerminalStatusManager : MonoBehaviour
     {
         if (!GameManager.isTerminalWorking)
         {
-            terminal.enabled = false;
-            typing.enabled = false;
-            notWorkingCoverImage.SetActive(true);
+            // terminal.enabled = false;
+            // typing.enabled = false;
+            foreach (GameObject obj in objectsToDisable)
+            {
+                obj.SetActive(false);
+            }
+            notWorkingStaticCoverImage.SetActive(true);
+            notWorkingBorderCoverImage.SetActive(true);
         }
         else
         {
-            terminal.enabled = true;
-            typing.enabled = true;
-            notWorkingCoverImage.SetActive(false);
+            // terminal.enabled = true;
+            // typing.enabled = true;
+            foreach (GameObject obj in objectsToDisable)
+            {
+                obj.SetActive(true);
+            }
+            notWorkingStaticCoverImage.SetActive(false);
+            notWorkingBorderCoverImage.SetActive(false);
         }
     }
 }
