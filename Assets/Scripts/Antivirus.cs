b918a6ac676271cf9cf2cb9661dedb2390cf5854
Andrew Romano <romanoa4@tcnj.edu>
2026-04-24 13:03:19 -0400

    Added Antivirus Progress Bar

--- Assets/Scripts/Antivirus.cs
+++ Assets/Scripts/Antivirus.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
+using UnityEngine.UI;
 
 public class Antivirus : MonoBehaviour
 {
     public TMP_Text issuesText;
+    public Image progressBar;
+    const float SCAN_TIME = 1f;
+    const float DISINFECT_TIME = 3f;
+    private bool isAntivirusActive = false;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,8 +24,21 @@ public class Antivirus : MonoBehaviour
 
     }
 
-    public void RunScan()
+    public IEnumerator RunScan()
     {
+        Debug.Log("Starting Antivirus Scan...");
+        isAntivirusActive = true;
+        issuesText.text = "Scanning...";
+
+        // Wait the time to scan
+        float t = 0f;
+        while (t < SCAN_TIME)
+        {
+            t += Time.deltaTime;
+            progressBar.fillAmount = t / SCAN_TIME;
+            yield return null;
+        }
+
         int issues = 0;
         issues += GameManager.moneyDrainAmount;
         string issuesColor = "green";
@@ -29,10 +49,35 @@ public class Antivirus : MonoBehaviour
         }
 
         issuesText.text = "<color=\"" + issuesColor + "\">" + issues + "</color> Issues Found";
+        Debug.Log("Scan Complete");
+        progressBar.fillAmount = 0;
+        isAntivirusActive = false;
+    }
+
+    public void ScanCoroutine()
+    {
+        if (!isAntivirusActive)
+        {
+            StartCoroutine("RunScan");
+        }
     }
     
-    public void DisinfectSystem()
+    public IEnumerator DisinfectSystem()
     {
+        Debug.Log("Starting Antivirus Disinfect...");
+        isAntivirusActive = true;
+        issuesText.text = "Disinfecting...";
+
+        // Wait the time to disinfect
+        float t = 0f;
+        while (t < DISINFECT_TIME)
+        {
+            t += Time.deltaTime;
+            progressBar.fillAmount = t / DISINFECT_TIME;
+            yield return null;
+        }
+
+
         int issues = 0;
         issues += GameManager.moneyDrainAmount;
         string issuesColor = "green";
@@ -43,5 +88,16 @@ public class Antivirus : MonoBehaviour
         GameManager.numHacksFailed = 0;
 
         issuesText.text = "<color=\"" + issuesColor + "\">" + issues + "</color> Issues Fixed";
+        Debug.Log("Disinfect Complete");
+        progressBar.fillAmount = 0;
+        isAntivirusActive = false;
+    }
+
+     public void DisinfectCoroutine()
+    {
+        if (!isAntivirusActive)
+        {
+            StartCoroutine("DisinfectSystem");
+        }
     }
 }
